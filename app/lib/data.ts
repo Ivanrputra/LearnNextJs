5eb94f0d408977d73f47790a2058a457a4b51b35
ivanrputra <ivanrputra@gmail.com>
2025-04-15 15:19:18 +0700

    -

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,12 +16,11 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -55,9 +54,9 @@ export async function fetchCardData() {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
-    const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
-    const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
-    const invoiceStatusPromise = sql`SELECT
+    const invoiceCountPromise   = sql`SELECT COUNT(*) FROM invoices`;
+    const customerCountPromise  = sql`SELECT COUNT(*) FROM customers`;
+    const invoiceStatusPromise  = sql`SELECT
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
@@ -68,10 +67,10 @@ export async function fetchCardData() {
       invoiceStatusPromise,
     ]);
 
-    const numberOfInvoices = Number(data[0][0].count ?? '0');
-    const numberOfCustomers = Number(data[1][0].count ?? '0');
-    const totalPaidInvoices = formatCurrency(data[2][0].paid ?? '0');
-    const totalPendingInvoices = formatCurrency(data[2][0].pending ?? '0');
+    const numberOfInvoices      = Number(data[0][0].count ?? '0');
+    const numberOfCustomers     = Number(data[1][0].count ?? '0');
+    const totalPaidInvoices     = formatCurrency(data[2][0].paid ?? '0');
+    const totalPendingInvoices  = formatCurrency(data[2][0].pending ?? '0');
 
     return {
       numberOfCustomers,
